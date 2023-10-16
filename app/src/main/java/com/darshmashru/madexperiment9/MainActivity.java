2845432b48505995849e54d71ef2ed7cf9aa9017
Darsh Mashru <mashrudarsh31@gmail.com>
2023-10-16 20:59:24 +0530

    Progress #2

--- app/src/main/java/com/darshmashru/madexperiment9/MainActivity.java
+++ app/src/main/java/com/darshmashru/madexperiment9/MainActivity.java
@@ -1,82 +1,74 @@
 package com.darshmashru.madexperiment9;
 
-
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import androidx.appcompat.content.res.AppCompatResources;
+
+import androidx.annotation.DrawableRes;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
+
 import com.mapbox.geojson.Point;
-import com.mapbox.mapboxsdk.maps.MapView;
-import com.mapbox.mapboxsdk.maps.Style;
-import com.mapbox.mapboxsdk.annotations.Annotation;
+import com.mapbox.maps.MapView;
+import com.mapbox.maps.Style;
+import com.mapbox.maps.plugin.annotation.AnnotationPlugin;
+import com.mapbox.maps.plugin.annotation.AnnotationPluginImplKt;
+import com.mapbox.maps.plugin.annotation.generated.PointAnnotationManager;
+import com.mapbox.maps.plugin.annotation.generated.PointAnnotationManagerKt;
 import com.mapbox.maps.plugin.annotation.generated.PointAnnotationOptions;
-import com.mapbox.maps.plugin.annotation.generated.createPointAnnotationManager;
+import com.mapbox.maps.plugin.delegates.MapPluginProviderDelegate;
 
 public class MainActivity extends AppCompatActivity {
-
     private MapView mapView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         mapView = findViewById(R.id.mapView);
-        mapView.getMapboxMap().loadStyleUri(
-                Style.MAPBOX_STREETS,
-                new Style.OnStyleLoaded() {
-                    @Override
-                    public void onStyleLoaded(Style style) {
-                        addAnnotationToMap();
-                    }
-                }
-        );
+        mapView.getMapboxMap().loadStyleUri(Style.MAPBOX_STREETS, new Style.OnStyleLoaded() {
+            @Override
+            public void onStyleLoaded(@NonNull Style style) {
+                addAnnotationToMap();
+            }
+        });
     }
 
     private void addAnnotationToMap() {
-        // Create an instance of the Annotation API and get the PointAnnotationManager.
         Bitmap bitmap = bitmapFromDrawableRes(this, R.drawable.red_marker);
         if (bitmap != null) {
-            annotations annotationsApi = mapView.annotations();
-            PointAnnotationManager pointAnnotationManager = annotationsApi.createPointAnnotationManager(mapView);
-            // Set options for the resulting symbol layer.
-            PointAnnotationOptions pointAnnotationOptions = new PointAnnotationOptions()
-                    .withPoint(Point.fromLngLat(18.06, 59.31))
+            AnnotationPlugin annotationplugin = AnnotationPluginImplKt.getAnnotations((MapPluginProviderDelegate) mapView);
+            PointAnnotationManager pointAnnotationManager;
+            pointAnnotationManager = PointAnnotationManagerKt.createPointAnnotationManager(annotationplugin, mapView);
+            PointAnnotationOptions options = new PointAnnotationOptions()
+                    .withPoint(Point.fromLngLat(19.107932578743736, 72.83720413753345))
                     .withIconImage(bitmap);
-            // Add the resulting pointAnnotation to the map.
-            pointAnnotationManager.create(pointAnnotationOptions);
+            pointAnnotationManager.create(options);
         }
     }
 
     private Bitmap bitmapFromDrawableRes(Context context, @DrawableRes int resourceId) {
-        return convertDrawableToBitmap(AppCompatResources.getDrawable(context, resourceId));
+        return convertDrawableToBitmap(ResourcesCompat.getDrawable(context.getResources(), resourceId, null));
     }
 
-    private Bitmap convertDrawableToBitmap(Drawable sourceDrawable) {
-        if (sourceDrawable == null) {
+    private Bitmap convertDrawableToBitmap(Drawable drawable) {
+        if (drawable == null) {
             return null;
-        }
-        if (sourceDrawable instanceof BitmapDrawable) {
-            return ((BitmapDrawable) sourceDrawable).getBitmap();
         }
 
-        // copying drawable object to not manipulate on the same reference
-        Drawable.ConstantState constantState = sourceDrawable.getConstantState();
-        if (constantState == null) {
-            return null;
+        if (drawable instanceof BitmapDrawable) {
+            return ((BitmapDrawable) drawable).getBitmap();
+        } else {
+            Bitmap bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+            Canvas canvas = new Canvas(bitmap);
+            drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+            drawable.draw(canvas);
+            return bitmap;
         }
-        Drawable drawable = constantState.newDrawable().mutate();
-        Bitmap bitmap = Bitmap.createBitmap(
-                drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight(),
-                Bitmap.Config.ARGB_8888
-        );
-        Canvas canvas = new Canvas(bitmap);
-        drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
-        drawable.draw(canvas);
-        return bitmap;
     }
 }
